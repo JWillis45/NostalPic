8cdc753f7fbcf3231e7a8155da9cc82b35eac899
JWillis45 <josephwillis@Josephs-MacBook-Pro.local>
2015-12-01 20:05:24 -0800

    setup image section and search by category section

--- index.php
+++ index.php
@@ -15,29 +15,11 @@ if (have_posts()) : while (have_posts()) : the_post();
       <p>Sorry, no pages matched your criteria.</p>
     <?php endif; wp_reset_postdata();?>
 
- -->
-
-<div class="row hd-image">
-    <div class="col-xs-12">
-      <div class="hd-search">
-          <?php get_search_form();?>
-        </div>
-    </div>
-  </div>
-
-
-
-<hr>
-
-<div class="row concrete">
-  <div class="col-xs-12">
-
-
-
-<?php 
+ --><?php 
 
 $args = array ('post_type' => 'photo',
-			   'posts_per_page' => '3'
+         'posts_per_page' => '1',
+         'orderby' => 'rand'
 );
 
 $photo = new WP_Query( $args );
@@ -48,28 +30,124 @@ if ($photo -> have_posts()) : while ($photo -> have_posts()) : $photo -> the_pos
 
 ?>
 
-<div id="box-wrap">
- 
-<div class="box<?php if( $count%3 == 0 ) { echo '-1'; }; $count++; ?>">
- 
-<a href="<?php the_permalink() ?>">
-          <img class="img-responsive" src="<?php the_field('image');?>" alt="">
-          <br>
-          <?php the_title();?>
-          <?php the_category(', '); ?>
+
+
+
+<!-- Search Form -->
+  <div class="row hd-image">
+    <div class="col-xs-12">
+      <div class="hd-search">
+        <?php get_search_form();?>
+      </div>
+    </div>
+  </div>
+<!-- End Search Form -->
+
+<hr>
+
+
+
+
+
+<div class="row concrete">
+  <div class="container TEMP">
+    <div class="row">
+      <div class="col-xs-7 home-thumbs-left">
+        <div class="row">
+          <div class="col-xs-6 home-thumbs">
+            <a href="<?php the_permalink() ?>">
+              <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/images/typewriter.png" alt="">
+            </a>
+          </div>
+          <div class="col-xs-6 home-thumbs2">
+              <a href="<?php the_permalink() ?>">
+              <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/images/truck.png" alt="">
+            </a>
+          </div>
+
+          <div class="col-xs-6 home-thumbs">
+            <a href="<?php the_permalink() ?>">
+              <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/images/coke.png" alt="">
+            </a>
+          </div>
+          <div class="col-xs-6 home-thumbs2">
+              <a href="<?php the_permalink() ?>">
+              <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/images/bike.png" alt="">
+            </a>
+          </div>
+        </div>
+      </div>
+
+    <div class="col-xs-4 home-thumbs-lg">
+      <a href="<?php bloginfo('url'); ?>/people">
+          <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/images/diner.png" alt="">
         </a>
-<!--and other output of the loop -->
- 
-</div>
- 
-<?php endwhile; ?>
-</div><!--end box-wrap-->
-</div><!-- end of column -->
-</div> <!-- end of row concrete -->
+
+     </div>
+  </div>
+  </div>
+</div><!-- end of row concrete -->
+
+<hr class = "hr2">
+
+<div class="row home-categories">
+  <div class="col-xs-12 categories-title">
+    <h1>Search By Category</h1>
+  </div>
+  
+  
+  <div class="col-xs-4">
+    <ul><a href="<?php bloginfo('url'); ?>/animals">Animals</a></ul>
+    <ul><a href="<?php bloginfo('url'); ?>/people-2">People</a></ul>
+    <ul><a href="<?php bloginfo('url'); ?>/food">Food</a></ul>
+    <ul><a href="<?php bloginfo('url'); ?>/illustrations">Illustrations</a></ul>
+    <ul><a href="<?php bloginfo('url'); ?>/toys">Toys</a></ul>
+    <ul><a href="<?php bloginfo('url'); ?>/music">Music</a></ul>
+  </div>
+  <div class="col-xs-4">
+    <ul><a href="<?php bloginfo('url'); ?>/people">Landmarks</a></ul>
+    <ul><a href="<?php bloginfo('url'); ?>/accessories">Accessories</a></ul>
+    <ul><a href="<?php bloginfo('url'); ?>/transportation">Transportation</a></ul>
+    <ul><a href="<?php bloginfo('url'); ?>/entertainment">Entertainment</a></ul>
+    <ul><a href="<?php bloginfo('url'); ?>/communications">Communications</a></ul>
+    <ul><a href="<?php bloginfo('url'); ?>/nature">Nature</a></ul>
+  </div>
+  <div class="col-xs-4">
+    <ul><a href="<?php bloginfo('url'); ?>/stationery">Stationery</a></ul>
+    <ul><a href="<?php bloginfo('url'); ?>/travel">Travel</a></ul>
+    <ul><a href="<?php bloginfo('url'); ?>/Buildings">Buildings</a></ul>
+    <ul><a href="<?php bloginfo('url'); ?>/industrial">Industrial</a></ul>
+    <ul><a href="<?php bloginfo('url'); ?>/business">Business</a></ul>
+    <ul><a href="<?php bloginfo('url'); ?>/typography">Typography</a></ul>
+  </div>
+</div><!-- end of row home-categories -->
 
 
+<!--
+<div class="row concrete">
+        
+          <div id="box-wrap">
+           
+          <div class="box<?php if( $count%3 == 0 ) { echo '-1'; }; $count++; ?>">
+           
+          <a href="<?php the_permalink() ?>">
+                    <img class="img-responsive" src="<?php the_field('image');?>" alt="">
+                    <br>
+                    <?php the_title();?>
+                    <?php the_category(', '); ?>
+                  </a>
+          <!-and other output of the loop ->
+           
+          </div>
+           
+          <?php endwhile; ?>
+          </div><!-end box-wrap->
+          </div>end of column
 
-<div class="row">
+-->
+
+
+<!-- <div class="row">
 
 <?php
         $defaults = array(
@@ -81,7 +159,7 @@ if ($photo -> have_posts()) : while ($photo -> have_posts()) : $photo -> the_pos
 
   <div class="col-xs-12 categories-title">
     <h2>Categories</h2>
-    <a href="<?php bloginfo('url'); ?>/posts-by-category">Landmarks</a>
+    <a href="<?php bloginfo('url'); ?>/people">Landmarks</a>
   </div>
   <div class="col-xs-4 categories">
 
@@ -90,6 +168,7 @@ if ($photo -> have_posts()) : while ($photo -> have_posts()) : $photo -> the_pos
   </div>
   
 </div>
+ -->
 
 
 
@@ -97,8 +176,7 @@ if ($photo -> have_posts()) : while ($photo -> have_posts()) : $photo -> the_pos
 
 
 
-
-
+<!-- 
 <div class="row">
   <div class="col-xs-12 lower-stuff">
       
@@ -110,7 +188,7 @@ if ($photo -> have_posts()) : while ($photo -> have_posts()) : $photo -> the_pos
         </div>
    </div>  
  		
- 	
+ 	 -->
 
 
       <p>Sorry, no pages matched your criteria.</p>
