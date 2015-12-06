c7d5125702a37bacc315c4e76a4c518979d13e66
JWillis45 <josephwillis@Josephs-MacBook-Pro.local>
2015-12-05 16:03:29 -0800

    setup footer nav

--- footer.php
+++ footer.php
@@ -2,6 +2,34 @@
 		<?php wp_footer(); ?>
 	</div>
       
-<!-- x content -->
+<div class="banner6">
+  <div class="container footer">
+    <div class="col-xs-3">
+      <p>2015 NostalPic</p>
+    </div>
+    <div class="col-xs-6 bottom-nav">
+
+    	<?php
+        $defaults = array(
+          'menu_class'      => 'nav nav-pills',
+);
+        wp_nav_menu( $defaults );
+?>
+
+    </div>
+    <div class="col-xs-3 social-media">
+      <a href="https://www.instagram.com/"><img src="<?php bloginfo ('template_directory'); ?>/images/instagram.png" alt=""></a>
+            <a href="https://www.facebook.com/"><img src="<?php bloginfo ('template_directory'); ?>/images/facebook.png" alt=""></a>
+            <a href="https://www.twitter.com/"><img src="<?php bloginfo ('template_directory'); ?>/images/twitter.png" alt=""></a>
+
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
 </body>
 </html>
