f385ba66941f07b2e7f5dae28e71b3ff5aeb5bbe
JWillis45 <josephwillis@Josephs-MacBook-Pro.local>
2015-12-06 21:15:05 -0800

    styled search page

--- search.php
+++ search.php
@@ -1,20 +1,57 @@
 <?php get_header(); ?>
 
 
-<h1 class = "search-results-headline">Search Results For: <?php the_search_query();?></h1>
 
-<?php 
-  if (have_posts()) : while (have_posts()) : the_post(); 
-?>
 
-  <a href="<?php the_permalink() ?>">
-      <img src="<?php the_field('image');?>" alt="">
-      <?php the_title();?>
-  </a>  
+
+
+<div class="banner8">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<h1 class = "search-results-headline">Results For: <?php the_search_query();?></h1>
+			</div>
+			<div class="col-xs-12">
+				<div id="column-wrap">
+
+					<?php $count = 0;
+					$args = array ('post_type' => 'photo',
+			        'posts_per_page' => '20',
+			        'orderby' => 'rand'
+);
+
+$photo = new WP_Query( $args );
+					 
+					while($photo -> have_posts()) : $photo -> the_post(); ?>
+					 
+					<div class="box<?php if( $count%3 == 0 ) { echo '-1'; }; $count++; ?>">
+					 
+					<a href="<?php the_permalink() ?>">
+				      <img src="<?php the_field('image');?>" alt="">
+				      
+			  	  </a>
+					<!--and other output of the loop -->
+					 
+					</div>
+					 
+					<?php endwhile; ?>
+				</div><!--end column-wrap-->
+			</div>
+			
+
+		</div>
+	</div>
+</div>
+
+
+
+
+
+  
   <br> 
 
-  <?php endwhile; else: ?>
+
     <p>Sorry, no posts matched your criteria.</p>
-<?php endif; wp_reset_postdata();?>
+<?php wp_reset_postdata();?>
 
 <?php get_footer(); ?>
