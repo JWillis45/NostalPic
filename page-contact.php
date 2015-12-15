56971427cd10278a19f9c7b9f635450500223b08
JWillis45 <josephwillis@Josephs-MacBook-Pro.local>
2015-12-14 17:37:10 -0800

    ree

--- page-contact.php
+++ page-contact.php
@@ -4,60 +4,18 @@ Template Name: Contact Page
 */
 ?>
 
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-<div class="contactUsBackground" style="font-family: 'Slabo 13px', serif;" >
-
-	<div class="row">
-		<div class="col-md-3 col-md-offset-2">
-					<div class="col-xs-offset-1 col-xs-10 col-sm-offset-1 col-sm-10 col-md-offset-1 col-md-10 col-lg-offset-1 col-lg-10 contactUsInfo" >
-						<div class="col-xs-offset-1 col-xs-10 col-sm-offset-1 col-sm-10 col-md-offset-1 col-md-10 info1">
-					<ul style="list-style-type:none; font-size:14px;">
-						<li>123 Fake St.</li>
-						<li>Northridge, CA</li>
-						<li>91324</li>
-						<li>(xxx)xxx-xxxx</li>
-
-						
-					</ul>
-						</div>
-		</div>
-		</div>
-			<div class="col-md-4 col-md-offset-1 ">
-				<div class="col-xs-offset-1 col-xs-10 col-sm-offset-1 col-sm-10 col-md-offset-3 col-md-7 col-lg-offset-1 col-lg-10 contactUsInfo2">
-					<div class="col-xs-offset-1 col-xs-10 col-sm-offset-1 col-sm-10 col-md-offset-1 col-md-10 col-lg-offset-1 col-lg-10 info2">
-				<div style="text-align:center;"><span style="font-size:20px; color:black;">Follow Us</span></div>
-				<ul style="list-style-type:none">
-					<li><span style="color:black;">Facebook:</span> www.facebook.com/Nostalpic</li>
-					<li><span style="color:black;">Instagram:</span> @nostalpic</li>
-					<li><span style="color:black;">Twitter:</span> @nostalpic</li>
-				</ul>
-					</div>
-			</div>
-			</div>
-	</div>	
-	<div class="row fifty">
-		 
-		<div class=" col-md-4 col-md-offset-4">
-			<div class="col-xs-offset-1 col-sm-offset-1 col-sm-10 col-md-10 col-md-offset-1 contactUsForm" style-"color:black;" >
-				
-				<div class="col-xs-offset-1 col-xs-10 col-sm-offset-1 col-sm-10 col-md-10 col-md-offset-1 info3">
-			
-		
-			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-			 	<?php the_content(); ?>
+  	<h1><?php the_title(); ?></h1>
+ 	<?php the_content(); ?>
 
 
-			  <?php endwhile; else: ?>
-			    <p>Sorry, no pages matched your criteria.</p>
-			<?php endif; ?>
-				</div>
-			
-		</div>
-		</div>
-		 
-	</div>
-</div>
+  <?php endwhile; else: ?>
+    <p>Sorry, no pages matched your criteria.</p>
+<?php endif; ?>
+
+
+
 
 <?php get_footer(); ?>
 
--- search.php
+++ search.php
@@ -2,6 +2,7 @@
 <div class="banner9">
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
 
@@ -9,6 +10,10 @@
 =======
 <?php $accord = 0; ?>
 >>>>>>> parent of 4b61243... changed search results page back to single column
+=======
+
+
+>>>>>>> parent of e3f1cfa... contact
 <div class="banner8 ">
 	<div class="container sr-bg">
 		<div class="row">
@@ -92,4 +97,50 @@
 
 
 <?php get_footer(); ?>
+<<<<<<< HEAD
 >>>>>>> parent of 4b61243... changed search results page back to single column
+=======
+=======
+	<?php $accord = 0; ?>
+	<div class="banner8 ">
+		<div class="container sr-bg">
+			<div class="row">
+				<h1 class = "search-results-headline">Search Results For: <?php the_search_query();?></h1>
+			</div>
+			<div class="row search-images">
+				<?php
+				  if (have_posts()) : while (have_posts()) : the_post();
+				?>
+				<div class="col-xs-4">
+					<div class="panel-group testing" id="accordion" role="tablist" aria-multiselectable="true">
+				  	<div class="panel panel-default">
+				  		<div class="panel-heading" role="tab" id="headingOne">
+								<a role="button" data-toggle="collapse" data-parent="#accordion" href="#<?php echo "thumbnail".$accord; ?>" aria-expanded="true" aria-controls="collapseOne">
+				          Collapsible Group Item #1
+				    			<div class="sr-image">
+										<div class="sr2">
+								  		<img class="img-responsive" src="<?php the_field('image');?>" alt="">
+							    	</div>
+							 		</div>
+								</a>
+				      	<h4 class="panel-title"></h4>
+				    	</div>
+					    <div id="<?php echo "thumbnail".$accord; $accord++; ?>" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+					      <div class="panel-body">
+					      	<?php the_title();?>
+					      </div>
+					    </div>
+				  	</div><!-- /panel-default -->
+					</div>
+				</div>
+				<?php endwhile; else: ?>
+				    <p>Sorry, no posts matched your criteria.</p>
+				<?php endif; wp_reset_postdata();?>
+			</div><!-- end row search images -->
+		</div><!-- end container -->
+	</div><!-- end banner 8 -->
+</div><!-- end banner 9 -->
+
+<?php get_footer(); ?>
+>>>>>>> origin/Home-Page
+>>>>>>> parent of e3f1cfa... contact
